19e509e24a1b20fd91c932bf33a9510a1a3e771a
zzqzsf <1014221651@qq.com>
2019-09-05 19:27:51 +0800

    Session Share

---
+++ src/main/java/org/lanqiao/sessionshare/SessionShareApplication.java
@@ -0,0 +1,13 @@
+package org.lanqiao.sessionshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionShareApplication.class, args);
+	}
+
+}
